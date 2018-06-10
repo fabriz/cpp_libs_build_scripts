6013a81ff1f2743de6f6501bc5a219ae9111f1fd
Fabrizio Maj <fabriz@users.noreply.github.com>
2018-06-10 19:22:37 +0100

    libical support - linux

--- libs/1900_build_ical.sh
+++ libs/1900_build_ical.sh
@@ -6,7 +6,26 @@ source "${THIS_SCRIPT_DIR}/../common.sh"
 
 buildCurrentArchitecture__linux_gcc()
 {
-    :
+    local BUILD_CONFIGURATION=""
+    if [ ${FM_TARGET_BUILD_VARIANT} = "debug" ]; then
+        BUILD_CONFIGURATION="Debug"
+    else
+        BUILD_CONFIGURATION="Release"
+    fi
+
+    prepareBuildStep "Configuring ${FM_CURRENT_ARCHITECTURE_LIB_TAG} ... "
+    cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=${BUILD_CONFIGURATION} -DSTATIC_ONLY=True\
+        -DCMAKE_DISABLE_FIND_PACKAGE_ICU=True -DWITH_CXX_BINDINGS=False -DGOBJECT_INTROSPECTION=False\
+        -DICAL_GLIB_VAPI=False -DICAL_GLIB=False -DCMAKE_PREFIX_PATH=${FM_LIBS_INSTALL_PREFIX} -DCMAKE_INSTALL_PREFIX=${FM_CURRENT_ARCHITECTURE_STAGE_DIR} > ${FM_CURRENT_ARCHITECTURE_LOG_FILE_CONFIGURE} 2>&1
+    checkBuildStep
+
+    prepareBuildStep "Building ${FM_CURRENT_ARCHITECTURE_LIB_TAG} ... "
+    make -j${FM_GLOBAL_NUM_PROCESSES} > ${FM_CURRENT_ARCHITECTURE_LOG_FILE_MAKE} 2>&1
+    checkBuildStep
+
+    prepareBuildStep "Staging ${FM_CURRENT_ARCHITECTURE_LIB_TAG} ... "
+    make install > ${FM_CURRENT_ARCHITECTURE_LOG_FILE_STAGE} 2>&1
+    checkBuildStep
 }
 
 buildCurrentArchitecture__macos_clang()
