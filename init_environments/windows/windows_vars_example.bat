686cc95a9c697fbb9a408f627034fdd3787e8c0d
Fabrizio Maj <fabriz@users.noreply.github.com>
2018-06-11 23:28:16 +0200

    Environment initialization scripts - Windows - mingw gcc

--- init_environments/windows/windows_vars_example.bat
+++ init_environments/windows/windows_vars_example.bat
@@ -4,6 +4,7 @@ set FM_GLOBAL_MSYS64_INSTALL_PATH=C:\msys64
 
 set FM_GLOBAL_MSVC2015_INSTALL_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
 set FM_GLOBAL_MSVC2017_INSTALL_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
+set FM_GLOBAL_MINGW_INSTALL_PATH=C:\Qt\Tools\mingw530_32
 
 
 set FM_GLOBAL_TARBALL_CACHE=/c/libs_build/cache
