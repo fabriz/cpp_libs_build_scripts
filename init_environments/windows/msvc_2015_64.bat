3ab231eaba3bd84c0cb3b9cc1ca83dfb6f03f4bc
Fabrizio Maj <fabriz@users.noreply.github.com>
2018-06-11 23:26:31 +0200

    Environment initialization scripts - Windows - MSVC 2015

---
+++ init_environments/windows/msvc_2015_64.bat
@@ -0,0 +1,26 @@
+@echo off
+
+set FM_SCRIPT_PATH=%~dp0
+
+set FM_GLOBAL_HOST=windows
+set FM_GLOBAL_COMPILER=msvc2015
+set FM_GLOBAL_ARCHITECTURE=x64
+
+call "windows_vars.bat"
+
+call "%FM_GLOBAL_MSVC2015_INSTALL_PATH%\VC\vcvarsall.bat" amd64
+
+set PATH=%FM_GLOBAL_CMAKE_INSTALL_PATH%\bin;%PATH%;%FM_GLOBAL_MSYS64_INSTALL_PATH%\usr\local\bin;%FM_GLOBAL_MSYS64_INSTALL_PATH%\usr\bin;%FM_GLOBAL_MSYS64_INSTALL_PATH%\bin;%FM_GLOBAL_MSYS64_INSTALL_PATH%\opt\bin
+
+cd /D %FM_SCRIPT_PATH%\..\..
+
+echo.
+echo To build all libraries:
+echo ./build_all.sh %FM_GLOBAL_HOST%_%FM_GLOBAL_COMPILER% %FM_GLOBAL_ARCHITECTURE% debug,release
+echo.
+echo To build a single library:
+echo cd libs
+echo ./nnnn_build_libname.sh %FM_GLOBAL_HOST%_%FM_GLOBAL_COMPILER% %FM_GLOBAL_ARCHITECTURE% debug,release
+echo.
+
+%FM_GLOBAL_MSYS64_INSTALL_PATH%\usr\bin\bash.exe
---
+++ init_environments/windows/windows_vars_example.bat
@@ -0,0 +1,10 @@
+
+set FM_GLOBAL_CMAKE_INSTALL_PATH=C:\Program Files\CMake
+set FM_GLOBAL_MSYS64_INSTALL_PATH=C:\msys64
+
+set FM_GLOBAL_MSVC2015_INSTALL_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+
+
+set FM_GLOBAL_TARBALL_CACHE=/c/libs_build/cache
+set FM_GLOBAL_BUILD_ROOT=/c/libs_build/build
+set FM_GLOBAL_DEPLOY_ROOT=/c/libs
