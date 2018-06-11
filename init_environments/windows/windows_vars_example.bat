b980b87d73734378d44c7f803a4824233bc378a4
Fabrizio Maj <fabriz@users.noreply.github.com>
2018-06-11 23:27:38 +0200

    Environment initialization scripts - Windows - MSVC 2017

--- init_environments/windows/windows_vars_example.bat
+++ init_environments/windows/windows_vars_example.bat
@@ -3,6 +3,7 @@ set FM_GLOBAL_CMAKE_INSTALL_PATH=C:\Program Files\CMake
 set FM_GLOBAL_MSYS64_INSTALL_PATH=C:\msys64
 
 set FM_GLOBAL_MSVC2015_INSTALL_PATH=C:\Program Files (x86)\Microsoft Visual Studio 14.0
+set FM_GLOBAL_MSVC2017_INSTALL_PATH=C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional
 
 
 set FM_GLOBAL_TARBALL_CACHE=/c/libs_build/cache
