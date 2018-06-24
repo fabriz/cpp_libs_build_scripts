58ba11f2aeb331aa0ce202ffffd8d27fb5c2ccfc
Fabrizio Maj <fabriz@users.noreply.github.com>
2018-06-24 21:55:44 +0100

    Android build support on Linux

--- init_environments/linux/vars_example.sh
+++ init_environments/linux/vars_example.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-#export FM_GLOBAL_ANDROID_NDK_ROOT=/home/fabrizio/Android/Sdk/ndk-bundle
-export FM_GLOBAL_ANDROID_NDK_HOST=linux-x86_64
-#export FM_GLOBAL_ANDROID_API_LEVEL=28
 export FM_GLOBAL_ANDROID_API_LEVEL=24
 export FM_GLOBAL_ANDROID_STANDALONE_NDK_INSTALL_PATH=/home/fabrizio/Android/standalone/arm-linux-androideabi_24
 
 
-export FM_GLOBAL_TARBALL_CACHE=/fabri_libs/libs_source
-export FM_GLOBAL_BUILD_ROOT=/fabri_libs/build
-export FM_GLOBAL_DEPLOY_ROOT=/fabri_libs/libs
-
-
+export FM_GLOBAL_TARBALL_CACHE=/home/fabrizio/libs/libs_source
+export FM_GLOBAL_BUILD_ROOT=/home/fabrizio/libs/build
+export FM_GLOBAL_DEPLOY_ROOT=/home/fabrizio/libs/libs
