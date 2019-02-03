45af42534bdeff52f6824bbfd25ad897659941ed
Fabrizio Maj <fabriz@users.noreply.github.com>
2019-02-03 15:57:41 +0100

    Wt support

--- libs/1800_build_wt.sh
+++ libs/1800_build_wt.sh
@@ -85,6 +85,7 @@ buildCurrentArchitecture__windows_msvc()
 {
     prepareBuildStep "Configuring ${FM_CURRENT_ARCHITECTURE_LIB_TAG} ... "
     cmake -G"NMake Makefiles" -DCMAKE_BUILD_TYPE=${FM_CMAKE_TARGET_VARIANT_BUILD_TYPE} -DSHARED_LIBS=False\
+        -DWT_BOOST_DISCOVERY=True -DBOOST_PREFIX=${FM_LIBS_INSTALL_PREFIX} -DBOOST_VERSION=${FM_BOOST_SHORT_VERSION//./_}\
         -DCONNECTOR_FCGI=True -DCONNECTOR_HTTP=True -DMULTI_THREADED=True\
         -DBUILD_EXAMPLES=False -DINSTALL_DOCUMENTATION=False -DENABLE_LIBWTTEST=False -DINSTALL_EXAMPLES=False -DINSTALL_RESOURCES=True\
         -DCMAKE_PREFIX_PATH=${FM_LIBS_INSTALL_PREFIX} -DCMAKE_INSTALL_PREFIX=${FM_CURRENT_ARCHITECTURE_STAGE_DIR} > ${FM_CURRENT_ARCHITECTURE_LOG_FILE_CONFIGURE} 2>&1
@@ -97,6 +98,8 @@ buildCurrentArchitecture__windows_msvc()
     prepareBuildStep "Staging ${FM_CURRENT_ARCHITECTURE_LIB_TAG} ... "
     nmake install > ${FM_CURRENT_ARCHITECTURE_LOG_FILE_STAGE} 2>&1
     checkBuildStep
+
+    /usr/bin/find ./src -name "*.pdb" -exec cp "{}" ${FM_CURRENT_ARCHITECTURE_STAGE_DIR}/lib/ ';'
 }
 
 
