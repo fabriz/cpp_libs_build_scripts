4eb2f2adf0bc45a96f98486c41b5f71fb85c7c15
Fabrizio Maj <fabriz@users.noreply.github.com>
2020-02-26 21:31:09 +0100

    Refactoring - Windows host support

--- init_environments/windows/msvc_2015_64.bat
+++ init_environments/windows/msvc_2015_64.bat
@@ -6,6 +6,7 @@ call "vars.bat"
 
 set FM_GLOBAL_HOST=windows
 set FM_GLOBAL_COMPILER=msvc2015
+set FM_GLOBAL_TOOLCHAIN=%FM_GLOBAL_HOST%_%FM_GLOBAL_COMPILER%
 set FM_GLOBAL_ARCHITECTURE=x64
 
 set PATH=%FM_GLOBAL_CMAKE_INSTALL_PATH%\bin;^
@@ -22,10 +23,10 @@ cd /D %FM_SCRIPT_PATH%\..\..
 
 echo.
 echo To build all libraries:
-echo ./build_libset.sh latest %FM_GLOBAL_HOST%_%FM_GLOBAL_COMPILER% %FM_GLOBAL_ARCHITECTURE% debug,release
+echo ./build_libset.sh latest debug,release
 echo.
 echo To build a single library:
-echo ./build_lib.sh "LIBNAME/LIBVERSION" %FM_GLOBAL_HOST%_%FM_GLOBAL_COMPILER% %FM_GLOBAL_ARCHITECTURE% debug,release
+echo ./build_lib.sh "LIBNAME/LIBVERSION" debug,release
 echo.
 
 %FM_GLOBAL_MSYS64_INSTALL_PATH%\usr\bin\bash.exe
